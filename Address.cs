c33a0adcdffd1de66224197ea939e245abac3619
MattFofo <matteo.segattini2@gmail.com>
2022-07-05 14:49:20 +0200

    Add project files.

---
+++ Address.cs
@@ -0,0 +1,34 @@
+﻿//In questo esercizio dovrete leggere un file CSV (che vi allego), non tanto differente da quanto visto nel live-coding in classe, e salvare tutti gli indirizzi contenuti al sul interno all’interno di una lista di oggetti istanziati a partire dalla classe Indirizzo.
+//Attenzione: gli ultimi 3 indirizzi presentano dei possibili “casi particolari” che possono accadere a questo genere di file: vi chiedo di pensarci e di gestire come meglio crediate queste casistiche.
+
+internal class Address
+{
+    public string Name { get; private set; }
+    public string Surname { get; private set; }
+    public string Street { get; private set; }
+    public string City { get; private set; }
+    public string Province { get; private set; }
+    public string Zip { get; private set; }
+
+    public Address(string name, string surname, string street, string city, string province, string zip)
+    {
+        this.Name = name;
+        this.Surname = surname;
+        this.Street = street;
+        this.City = city;
+        this.Province = province;
+        this.Zip = zip;
+    }
+
+    public void PrintAddress()
+    {
+        Console.WriteLine("------ Address ------");
+        Console.WriteLine("Name: " + this.Name);
+        Console.WriteLine("Surname: " + this.Surname);
+        Console.WriteLine("Street: " + this.Street);
+        Console.WriteLine("City: " + this.City);
+        Console.WriteLine("Province: " + this.Province);
+        Console.WriteLine("Zip: " + this.Zip);
+        Console.WriteLine("---------------------");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿//In questo esercizio dovrete leggere un file CSV (che vi allego), non tanto differente da quanto visto nel live-coding in classe, e salvare tutti gli indirizzi contenuti al sul interno all’interno di una lista di oggetti istanziati a partire dalla classe Indirizzo.
+//Attenzione: gli ultimi 3 indirizzi presentano dei possibili “casi particolari” che possono accadere a questo genere di file: vi chiedo di pensarci e di gestire come meglio crediate queste casistiche.
+
+StreamReader addressesFromFile = File.OpenText("D:\\VisualStudio2022\\Experis\\csharp-lista-indirizzi\\addresses.csv");
+addressesFromFile.ReadLine();
+
+
+List<Address> addresses = new List<Address>();  
+
+//ciclo le linee del file
+while (!addressesFromFile.EndOfStream)
+{
+    string line = addressesFromFile.ReadLine();
+    Console.WriteLine(line);
+    string[] data = line.Split(",");
+
+    string name = data[0];
+    string surname = data[1];
+    string street = data[2];
+    string city = data[3];
+    string province;
+    string zip;
+    try
+    {
+        province = data[4];
+    }
+    catch (Exception exc)
+    {
+
+        province = "ND";
+    }
+
+    try
+    {
+        zip = data[5];
+    }
+    catch (Exception exp)
+    {
+
+        zip = "ND";
+    }
+
+    
+
+    Address address = new Address(name, surname, street, city, province, zip);
+    addresses.Add(address);
+}
+
+addressesFromFile.Close();
+
+//stampo gli indirizzi
+foreach (var address in addresses)
+{
+    address.PrintAddress();
+}
